26c450c2024495d979337f5dcbffbcb6dce750ca
AdrianJS88 <113306625+AdrianJS88@users.noreply.github.com>
2023-07-27 23:30:58 +0300

    Initial commit

---
+++ src/main/java/com/ltp/gradesubmission/service/GradeServiceImpl.java
@@ -0,0 +1,78 @@
+package com.ltp.gradesubmission.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.ltp.gradesubmission.entity.Course;
+import com.ltp.gradesubmission.entity.Grade;
+
+import com.ltp.gradesubmission.entity.Student;
+import com.ltp.gradesubmission.exception.GradeNotFoundException;
+import com.ltp.gradesubmission.repository.CourseRepository;
+import com.ltp.gradesubmission.repository.GradeRepository;
+import com.ltp.gradesubmission.repository.StudentRepository;
+import lombok.AllArgsConstructor;
+
+
+import org.springframework.stereotype.Service;
+@AllArgsConstructor
+@Service
+public class GradeServiceImpl implements GradeService {
+
+    StudentRepository studentRepository;
+    GradeRepository gradeRepository;
+    CourseRepository courseRepository;
+
+    @Override
+    public Grade getGrade(Long studentId, Long courseId) {
+        Optional<Grade> grade = gradeRepository.findByStudentIdAndCourseId(studentId, courseId);
+        return unwrapGrade(grade, studentId, courseId);
+    }
+
+    @Override
+    public Grade saveGrade(Grade grade, Long studentId, Long courseId) {
+        Student student = StudentServiceImpl.unwrapStudent(studentRepository.findById(studentId), studentId);
+        Course course = CourseServiceImpl.unwrapCourse(courseRepository.findById(courseId), courseId);
+        grade.setStudent(student);
+        grade.setCourse(course);
+        return gradeRepository.save(grade);
+    }
+
+    @Override
+    public Grade updateGrade(String score, Long studentId, Long courseId) {
+        Optional<Grade> grade = gradeRepository.findByStudentIdAndCourseId( studentId, courseId );
+        if (grade.isPresent()) {
+            Grade unweappedGrade = grade.get();
+            unweappedGrade.setScore( score );
+            return  gradeRepository.save( unweappedGrade );
+
+        } else {
+            throw new GradeNotFoundException( studentId, courseId );
+        }
+    }
+
+
+    @Override
+    public void deleteGrade(Long studentId, Long courseId) {
+        gradeRepository.deleteByStudentIdAndCourseId(studentId, courseId);
+    }
+
+    @Override
+    public List<Grade> getStudentGrades(Long studentId) {
+        return gradeRepository.findByStudentId( studentId );
+    }
+
+    @Override
+    public List<Grade> getCourseGrades(Long courseId) {
+        return gradeRepository.findByCourseId( courseId );
+    }
+
+    @Override
+    public List<Grade> getAllGrades() {
+        return (List<Grade>) gradeRepository.findAll();
+    }
+    static Grade unwrapGrade(Optional<Grade> entity, Long studentId, Long courseId) {
+        if (entity.isPresent()) return entity.get();
+        else throw new GradeNotFoundException(studentId, courseId);
+    }
+}
